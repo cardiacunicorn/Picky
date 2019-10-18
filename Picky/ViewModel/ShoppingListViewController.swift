ed6e1f667dc64a4403bac449c4b4331529c22c2f
Alex Mills <alexanderwmills@gmail.com>
2019-10-18 23:21:34 +0900

    Working...ish - the shopping list loads, but not before the view is rendered

--- Picky/ViewModel/ShoppingListViewController.swift
+++ Picky/ViewModel/ShoppingListViewController.swift
@@ -12,6 +12,7 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
     
     private let viewModel = ShoppingViewModel()
 
+    @IBOutlet weak var tableView: UITableView!
     @IBAction func addItemButton(_ sender: Any) {
         // Triggered when the user taps the + button
     }
@@ -19,27 +20,27 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
         // Triggered when the user finishes editing their input
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Shopping List has loaded and is being viewed.")
+        // Tells the TableView this file will be its delegate & datasource
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("Anyone home?")
         return 8
     }
     
+    // ISSUE: This function does not appear to run before the view is loaded
+    // Consider whether preparing for a segue from the TabBar may help
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        // Code is not reaching this point
-        
-        print("Anyone home?")
         let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingItem", for: indexPath)
-        
+
         let listItem = cell.viewWithTag(1010) as? UIButton
-        print(listItem?.titleLabel?.text)
-        
+
         if let listItem = listItem {
             let currentItem = viewModel.getItem(byIndex: indexPath.row)
             listItem.titleLabel?.text = currentItem
