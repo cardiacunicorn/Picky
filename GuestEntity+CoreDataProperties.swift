028269d84be747802fb24f7b976b599a08e79b11
Alex Mills <alexanderwmills@gmail.com>
2019-11-16 19:37:01 +0300

    Reworked Data Model

--- GuestEntity+CoreDataProperties.swift
+++ GuestEntity+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  GuestEntity+CoreDataProperties.swift
 //  Picky
 //
-//  Created by Alex Mills on 15/11/19.
+//  Created by Alex Mills on 16/11/19.
 //  Copyright © 2019 Alex Mills. All rights reserved.
 //
 //
@@ -17,9 +17,26 @@ extension GuestEntity {
         return NSFetchRequest<GuestEntity>(entityName: "GuestEntity")
     }
 
-    @NSManaged public var allergies: NSObject?
-    @NSManaged public var diets: NSObject?
+    @NSManaged public var allergies: [String]?
+    @NSManaged public var diets: [String]?
     @NSManaged public var name: String?
-    @NSManaged public var guestlists: GuestlistEntity?
+    @NSManaged public var guestlists: NSSet?
+
+}
+
+// MARK: Generated accessors for guestlists
+extension GuestEntity {
+
+    @objc(addGuestlistsObject:)
+    @NSManaged public func addToGuestlists(_ value: GuestlistEntity)
+
+    @objc(removeGuestlistsObject:)
+    @NSManaged public func removeFromGuestlists(_ value: GuestlistEntity)
+
+    @objc(addGuestlists:)
+    @NSManaged public func addToGuestlists(_ values: NSSet)
+
+    @objc(removeGuestlists:)
+    @NSManaged public func removeFromGuestlists(_ values: NSSet)
 
 }
--- GuestlistEntity+CoreDataProperties.swift
+++ GuestlistEntity+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  GuestlistEntity+CoreDataProperties.swift
 //  Picky
 //
-//  Created by Alex Mills on 15/11/19.
+//  Created by Alex Mills on 16/11/19.
 //  Copyright © 2019 Alex Mills. All rights reserved.
 //
 //
@@ -17,9 +17,26 @@ extension GuestlistEntity {
         return NSFetchRequest<GuestlistEntity>(entityName: "GuestlistEntity")
     }
 
-    @NSManaged public var allergies: NSObject?
-    @NSManaged public var diets: NSObject?
+    @NSManaged public var allergies: [String]?
+    @NSManaged public var diets: [String]?
     @NSManaged public var name: String?
-    @NSManaged public var guests: GuestEntity?
+    @NSManaged public var guests: NSSet?
+
+}
+
+// MARK: Generated accessors for guests
+extension GuestlistEntity {
+
+    @objc(addGuestsObject:)
+    @NSManaged public func addToGuests(_ value: GuestEntity)
+
+    @objc(removeGuestsObject:)
+    @NSManaged public func removeFromGuests(_ value: GuestEntity)
+
+    @objc(addGuests:)
+    @NSManaged public func addToGuests(_ values: NSSet)
+
+    @objc(removeGuests:)
+    @NSManaged public func removeFromGuests(_ values: NSSet)
 
 }
